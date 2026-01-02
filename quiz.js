6dde37e7621c3f22647e36125f63a3f4af60abc9
Prajwal-Shrestha <prajwalshrestha2468@gmail.com>
2026-01-02 09:51:20 +0545

    feat:fetching question from api

--- quiz.js
+++ quiz.js
@@ -2,3 +2,19 @@ const API_BASE_URL = 'https://opentdb.com/api.php';
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get('category');
 const difficulty = urlParams.get('difficulty');
+
+function fetchQuizQuestions() {
+    let apiUrl = `${API_BASE_URL}?amount=25&type=multiple`;
+
+    if (category) {
+        apiUrl += `&category=${category}`;
+    }
+
+    if (difficulty) {
+        apiUrl += `&difficulty=${difficulty}`;
+    }
+
+    console.log('Fetching questions from URL:', apiUrl);
+}
+fetchQuizQuestions();
+
